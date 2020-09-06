f07e44d050078aa72c944ed23016cacef2a90f3c
Hans Ulli Kroll <linux@ulli-kroll.de>
2020-09-06 13:44:36 +0200

    remove unused struct usb_device_info
    Signed-off-by: Hans Ulli Kroll <linux@ulli-kroll.de>

--- compatibility.h
+++ compatibility.h
@@ -10,12 +10,4 @@
 	#define FALSE	0
 #endif
 
-struct usb_device_info {
-	__u16		idVendor;
-	__u16		idProduct;
-	__u16		bcdDevice;
-	__u8		dev_addr[8];
-	char		devpath[16];
-};
-
 #endif /* LINUX_COMPATIBILITY_H */
